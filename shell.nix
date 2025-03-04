28587d01042e7f44327a7606d5debe62f36cc5dc
Lawrence Logoh <lawrence.logoh@gmail.com>
2025-03-04 21:34:37 +0000

    Add github CI/CD script

--- shell.nix
+++ shell.nix
@@ -1,10 +1,10 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import (fetchTarball "https://nixos.org/channels/nixos-24.05/nixexprs.tar.xz") {} }:
 
 pkgs.mkShell {
-
   buildInputs = [
     pkgs.poetry
     pkgs.python312
+    pkgs.act
   ];
 
 }
