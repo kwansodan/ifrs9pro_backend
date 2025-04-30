6414bf57c703a4c4e1057181ea50e8bfa403dbb4
Clinton <kwansodan@gmail.com>
2025-04-30 13:17:57 +0100

    latest changes

--- app/utils/background_calculations.py
+++ app/utils/background_calculations.py
@@ -272,7 +272,8 @@ async def process_bog_impairment_calculation_sync(portfolio_id: int, reporting_d
         loss_rate = Decimal(provision_config.get("loss", {}).get("rate", 0))
 
 
-        #fethc all loans in the portfolio
+        #fethc all active loans in the portfolio
+        
         loans = db.query(Loan).filter(Loan.portfolio_id == portfolio_id).order_by(Loan.id).offset(offset).limit(batch_size).all()
         if not loans:
             break  # No more loans to process
