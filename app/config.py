dc1f2417da18961c6159a540951bd4db772c413b
Clinton <kwansodan@gmail.com>
2025-05-08 12:39:10 +0100

    updates in reporting

--- app/config.py
+++ app/config.py
@@ -1,6 +1,8 @@
 from dotenv import load_dotenv
 import os
 from app.utils.db import convert_libpq_to_sqlalchemy
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
+import os
 
 load_dotenv()  # Load environment variables from .env
 load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '..', '.env'))
@@ -13,5 +15,7 @@ class Settings:
     ACCESS_TOKEN_EXPIRE_HOURS: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_HOURS", "24"))
     ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "30"))
     SQLALCHEMY_DATABASE_URL: str = os.getenv("SQLALCHEMY_DATABASE_URL")
+    AZURE_STORAGE_CONNECTION_STRING = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
+    CONTAINER_NAME = os.getenv("CONTAINER_NAME")
 
 settings = Settings()
