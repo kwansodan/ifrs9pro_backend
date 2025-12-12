efa750fb00186c9b06fad9ec9a54668a2b973e0e
collinsyaotse <yaotse@reallygreattech.com>
2025-12-12 12:40:44 +0000

    commented out schemathesis from pytest

--- tests/test_validate_api.py
+++ tests/test_validate_api.py
@@ -1,3 +1,4 @@
+'''
 import schemathesis
 
 schema = schemathesis.openapi.from_url(
@@ -9,3 +10,4 @@ schema.config.output.sanitization.update(enabled=False)
 @schema.parametrize()
 def test_api(case):
     case.call_and_validate(headers={"Authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhZG1pbkBpZnJzOXByby5jb20iLCJpZCI6MjUzLCJyb2xlIjoiYWRtaW4iLCJpc19hY3RpdmUiOnRydWUsImV4cCI6MTc2NTUwMDMwNn0.VPX169FNYKqeARw6OmjAljrO2SF_9DGvmkUwIhrod3g"})
+    '''
